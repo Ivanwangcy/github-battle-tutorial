cec2688456dbbcf014a5c81e3b26166e15398a64
Ivan <wcy5211678@163.com>
2016-05-14 21:46:39 +0800

    add github userinfo

--- app/index.js
+++ app/index.js
@@ -1,17 +1,85 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Hello extends React.Component {
+/**
+ * [USER_DATA Github 用户数据]
+ * @type {Array} ｛name, username, image}
+ */
+let USER_DATA = [
+  {
+    name: 'Tyler McGinnis',
+    username: 'tylermcginnis',
+    image: 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'
+  },
+  {
+    name: 'Ruan YiFeng',
+    username:'ruanyf',
+    image: 'https://avatars3.githubusercontent.com/u/905434?v=3&s=460',
+  },
+  {
+    name: 'Ivan',
+    username: 'Ivanwangcy',
+    image: 'https://avatars3.githubusercontent.com/u/6278969?v=3&s=460'
+  }
+];
+
+/**
+ * 简介 头像
+ */
+class ProfilePic extends React.Component {
+  render(){
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}}/>
+  }
+}
+
+/**
+ * 简介 链接
+ */
+class ProfileLink extends React.Component {
   render(){
     return (
       <div>
-        Hello World! Hello {this.props.year}! Hello {this.props.name}!
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
       </div>
+    );
+   }
+}
+
+/**
+ * 简介 姓名
+ */
+class ProfileName extends React.Component {
+  render(){
+    return <div>{this.props.name}</div>;
+  }
+}
+
+/**
+ * github 个人简介
+ */
+class Avatars extends React.Component {
+  render() {
+    console.log(this.props);
+    var avatarList = this.props.users.map((value) => {
+      return (
+        <li key={value.username}>
+          <ProfilePic imageUrl={value.image}></ProfilePic>
+          <ProfileName name={value.name}></ProfileName>
+          <ProfileLink username={value.username}></ProfileLink>
+        </li>
+      );
+    });
+    return (
+      <ul>
+        {avatarList}
+      </ul>
     );
   }
 }
 
 ReactDOM.render(
-  <Hello name="Ivan" year={2016} />,
+  <Avatars users={USER_DATA} />,
   document.getElementById('app')
 );
