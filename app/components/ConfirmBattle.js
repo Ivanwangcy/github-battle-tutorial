4d0fee74c2f5174032dce3b17fdcef0d1bc79e99
Ivan <wcy5211678@163.com>
2016-05-21 17:18:40 +0800

    confirm battle

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,1 +1,15 @@
-import React from 'react';
+import React, {Component, PropTypes} from 'react';
+
+function ConfirmBattle (prop) {
+  return (
+    prop.isLoading === true ?
+    <div>Loding</div> :
+    <div>DidMount</div>
+  );
+}
+
+ConfirmBattle.PropTypes = {
+  'isLoading': PropTypes.string.isRequired
+}
+
+export default ConfirmBattle;
--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,6 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import ConfirmBattle from './comonents/ConfirmBattle';
 
-ReactDOM.render(
-  <div></div>
-)
+class ConfirmBattleContainer extends ConfirmBattle {
+  constructor() {
+    this.state = {
+      isLoading: true,
+      playersInfo: []
+    }
+  }
+
+  componentDidMount() {
+    var query = this.props.location.query;
+
+    console.log(query);
+  }
+
+  render() {
+    return (
+      <ConfirmBattle
+        isLoading={this.state.loading}
+        playersInfo={this.state.playersInfo}
+      />
+    )
+  }
+}
+
+ConfirmBattleContainer.contextTypes = {
+  router: PropTypes.object.isRequired
+}
+
+export default ConfirmBattleContainer;
