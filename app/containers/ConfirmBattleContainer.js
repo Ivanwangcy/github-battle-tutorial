501aa6371d9894bcb42eb565dfc68f93c9cc3cb5
Ivan <wcy5211678@163.com>
2016-06-07 09:38:05 +0800

    container

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -10,7 +10,7 @@ class ConfirmBattleContainer extends Component {
       isLoading: true,
       playersInfo: []
     }
-    this.handleInitiateBattle = this.handleInitiateBattle.bind(this);  
+    this.handleInitiateBattle = this.handleInitiateBattle.bind(this);
     console.log("Component initial");
   }
 
