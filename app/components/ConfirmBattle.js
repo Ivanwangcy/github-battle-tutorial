9a37dc4c9448caad745c89dbc296d4c9ca03d82a
Ivan <wcy5211678@163.com>
2016-06-14 06:49:27 +0800

    confirm results

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -4,16 +4,17 @@ import { Link } from 'react-router';
 import UserDetails from './UserDetails';
 import UserDetailsWrapper from './UserDetailsWrapper';
 
-function puke (object) {
-  return <pre>{JSON.stringify(object, null, "\t")}</pre>; // 格式化输出
-}
+// function puke (object) {
+//   return <pre>{JSON.stringify(object, null, "\t")}</pre>; // 格式化输出
+// }
+
 function ConfirmBattle (props) {
   return (
     props.isLoading === true
     ? <p>Loding! </p>
     : <div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
         <h1>Confirm Players</h1>
-        <div className='col-sm-8col-sm-offset-2'>
+        <div className='col-sm-8 col-sm-offset-2'>
               {/*{puke(props.playersInfo[0])}*/}
             <UserDetailsWrapper header='Player One'>
               <UserDetails info={props.playersInfo[0]} />
--- app/components/Results.js
+++ app/components/Results.js
@@ -1,17 +1,35 @@
 import React, {PropTypes} from 'react';
-
-function puke (object) {
-  return <pre>{JSON.stringify(object, null, "\t")}</pre>; // 格式化输出
-}
+import styles from '../styles/index';
+import { Link } from 'react-router';
+import UserDetails from './UserDetails';
+import UserDetailsWrapper from './UserDetailsWrapper';
+// function puke (object) {
+//   return <pre>{JSON.stringify(object, null, "\t")}</pre>; // 格式化输出
+// }
 
 function Results (props) {
-  return (
-    props.isLoading === true
-      ? <div>Loading ...</div>
-      : <div>{puke(props)} </div>
+  let [winningIndex, losingIndex] = props.scores[0] > props.scores[1] ? [0,1] : [1,0];
 
+  return (
+    <div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
+      <h1>Results</h1>
+      <div className='col-sm-8 col-sm-offset-2'>
+        <UserDetailsWrapper header='Winner'>
+          <UserDetails score={props.scores[winningIndex]} info={props.playersInfo[winningIndex]} />
+        </UserDetailsWrapper>
+        <UserDetailsWrapper header='Loser'>
+          <UserDetails score={props.scores[losingIndex]} info={props.playersInfo[losingIndex]} />
+        </UserDetailsWrapper>
+      </div>
+      <div className='col-sm-12' style={styles.space}>
+        <Link to='/playerone'>
+          <button className="btn btn-lg btn-danger">start Over</button>
+        </Link>
+      </div>
+    </div>
   );
 }
+
 
 Results.propTypes = {
   isLoading: PropTypes.bool.isRequired,
